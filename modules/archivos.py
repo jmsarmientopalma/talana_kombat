d2d3e6df131a7b57aeaf643ce011e4add67a088a
JM Sarmiento <jmsarmientopalma@gmail.com>
2023-02-13 07:31:44 -0400

    archivos adicionales

--- modules/archivos.py
+++ modules/archivos.py
@@ -1,7 +1,7 @@
-def lista_json():
+def lista_json(ruta_json):
     from pathlib import Path
     archivos_json = []
-    basepath = Path('json/')    
+    basepath = Path(ruta_json)    
     json_files = (archivo for archivo in basepath.iterdir() if archivo.is_file())
     for item in json_files:
         archivos_json.append(item.name)
